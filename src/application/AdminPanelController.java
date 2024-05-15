78fd61340bd7c49fb96233829c11a52165d65a6a
GyoElPipa <giorgio.miselli@studio.unibo.it>
2024-05-15 16:24:45 +0200

    Login Rework 1

--- src/application/AdminPanelController.java
+++ src/application/AdminPanelController.java
@@ -2,14 +2,24 @@ package application;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class AdminPanelController {
+public class AdminPanelController implements Initializable{
 	
 	private Stage stage;
 	private Main main;
+	@FXML
+	private Label LabelStatistiche;
+	@FXML
+	private TextField Usernamechange,Passwordchange;
 	
 	public void setMain(Main main) {
     	this.main= main;
@@ -19,27 +29,51 @@ public class AdminPanelController {
 			this.stage= stage;
 	}
 	
+	private int Npartite;
+	
 	//Per eliminare tutti i dati salvati
-		public void Pulisci(ActionEvent event) throws IOException {
-			File dir = new File("src/Salvataggi/");
-			String[] n = dir.list();
-				if(dir.isDirectory() && dir.exists()) {
-					for(int i = 0;i<n.length;i++) {
-						File f = new File("src/Salvataggi/"+n[i]);
-						f.delete();
-					}
+	public void Pulisci(ActionEvent event) throws IOException {
+		File dir = new File("src/Salvataggi/");
+		String[] n = dir.list();
+			if(dir.isDirectory() && dir.exists()) {
+				for(int i = 0;i<n.length;i++) {
+					File f = new File("src/Salvataggi/"+n[i]);
+					f.delete();
 				}
-			//Resetta il file che contiene le informazioni dei giocatori
-			ClassificaController.elimina();
+			}
+		//Resetta il file che contiene le informazioni dei giocatori
+		ClassificaController.elimina();
+		Npartite = 0;
+		Scrivistats();
+	}
+	
+	//Per scrivere le statistiche e al caso aggiornarle
+	public void Scrivistats() {
+		LabelStatistiche.setText("Numero di giocatori registrati: "+ClassificaController.LSGiocatori.size()+"\n"+
+				 "Numero di partite salvate: "+Npartite+"\n");
+	}
+	
+	public void CambioAccesso(ActionEvent event) {
+		String Newusername, Newpassword;
+		Newusername = Usernamechange.getText();
+		Newpassword = Passwordchange.getText();
+			LoginController.accesso.clear();
+			LoginController.accesso.put("1", "2");
 		}
+		
 	
 	//Per tornare al menuprincipale
 	public void Backtomenu(ActionEvent event) throws IOException {
 		try {
-			main.showMainMenuScene();;
+			main.showMainMenuScene();
 		} catch (Exception e) {
 			System.out.println("errore nel caricamento del main menu");
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Scrivistats();
 	}
 }
--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,29 +1,38 @@
 package application;
 
 import java.io.IOException;
+import java.io.Serializable;
+import java.net.URL;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import java.util.HashMap;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable,Serializable{
 //Classe per controllare la pagina di Login
+	
+	//Per serilizzazione dei dati di accesso
+	private static final long serialVersionUID = 5620015872095426512L;
 	
 	private Main main;
 	//Crea le istanze per prendere in input Nome e Password
 	@FXML
 	private TextField inputnome;
 	@FXML
-	private TextField inputpassword;
+	private PasswordField inputpassword;
 	@FXML
 	private Button tastoconferma;
 	@FXML
 	private Label errori;
 	
 	//Hashmap per avere username e password per l'admin
-	HashMap<String,String> accesso = new HashMap<String,String>();
+	public static HashMap<String,String> accesso = new HashMap<String,String>();
 	
 	String Nome;
 	String Password;
@@ -37,11 +46,8 @@ public class LoginController {
 
 	public void conferma(ActionEvent event) throws IOException, InterruptedException {
 		//Modificare qui per decidere nome utente e password dell'admin
-		accesso.put("", "");
 		Nome = inputnome.getText();
 		Password = inputpassword.getText();
-		HashMap<String,String> login = new HashMap<String,String>();
-		login.put(Nome,Password);
 		if(accesso.containsValue(Password) && accesso.containsKey(Nome)) {
 			 try {
 				main.showMainMenuScene();
@@ -53,6 +59,10 @@ public class LoginController {
 			
 		}else
 			errori.setText("USERNAME O PASSWORD ERRATI");
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
 }
 	
