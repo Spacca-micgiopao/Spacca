f9ec5cb9f802ae4b67801978bbd76c8fe2d8df33
polovit <151672469+polovit@users.noreply.github.com>
2024-05-22 12:53:27 +0200

    Update botLogic.java

--- src/application/botLogic.java
+++ src/application/botLogic.java
@@ -5,22 +5,31 @@ import java.util.Random;
 import javafx.scene.image.ImageView;
 
 public class botLogic {
-	public boolean giocaCarta(Mazzo mazzo, ArrayList<ImageView> tavolo) {
-        Random random = new Random();
-        int index = random.nextInt(mazzo.getCarte().size());
-        Carta cartaSelezionata = mazzo.getCarta(index);
+	public Carta giocaCarta(Mazzo mazzo, ArrayList<ImageView> tavolo) {
+	    Random random = new Random();
+	    int index = random.nextInt(mazzo.getCarte().size());
+	    Carta cartaSelezionata = mazzo.getCarta(index);
 
-        // Controlla se il tavolo ha spazio prima di giocare 
-        for (ImageView imageView : tavolo) {
-            if (imageView.getImage() == null) {
-                imageView.setImage(cartaSelezionata.getImmagine());
-                mazzo.rimuoviCarta(cartaSelezionata);
-                
-                return true;
-            }
-        
-      
-    } 
-      return false;
-}
+	    // Controlla se il tavolo ha spazio prima di giocare 
+	    for (ImageView imageView : tavolo) {
+	        if (imageView.getImage() == null) {
+	            // Imposta l'immagine dell'ImageView con quella della carta selezionata
+	            imageView.setImage(cartaSelezionata.getImmagine());
+	            
+	            // Assegna la carta selezionata all'attributo userData dell'ImageView
+	            imageView.setUserData(cartaSelezionata);
+	            
+	            // Rimuovi la carta dal mazzo
+	            mazzo.rimuoviCarta(cartaSelezionata);
+	            
+	            // Restituisci la carta giocata
+	            return cartaSelezionata;
+	        }
+	    }
+	    
+	    // Se non è possibile giocare la carta, restituisci null
+	    return null;
+	}
+
+
 }
